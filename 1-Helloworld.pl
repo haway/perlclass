6a2e080962c8a19983321f62f83c0d5bd4931fcf
HaWay <haway.tw@gmail.com>
2011-10-07 12:03:40 +0800

    init

---
+++ 1-Helloworld.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+#
+# 註解
+
+# 一般情況下不用引入任何東西。
+
+print "沒有分行的 Hello World";
+print "第二個 Hello World";
+
+# 分行跟 C++ 一樣 用 \n 
+print "\n\n\n有分行的 第一個 Hello World\n";
+print "第二個 Hello World\n";
+# print 可以分段
+print "第三個 Hello World", "\n";
+print "第四個 Hello World", "\n";
